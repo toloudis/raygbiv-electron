1dfb0134590c7558b9d7e3f9cbb245e8cc49229b
Dan Toloudis <dmt.reg@gmail.com>
2020-08-16 15:31:23 -0700

    initial add

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = [
+  {
+    mode: "development",
+    entry: "./src/main.ts",
+    target: "electron-main",
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          include: /src/,
+          use: [{ loader: "ts-loader" }],
+        },
+      ],
+    },
+    output: {
+      path: __dirname + "/dist",
+      filename: "main.js",
+    },
+  },
+  {
+    mode: "development",
+    entry: "./src/index.ts",
+    target: "electron-renderer",
+    devtool: "source-map",
+    module: {
+      rules: [
+        {
+          test: /\.ts(x?)$/,
+          include: /src/,
+          use: [{ loader: "ts-loader" }],
+        },
+      ],
+    },
+    output: {
+      path: __dirname + "/dist",
+      filename: "index.js",
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./src/index.html",
+      }),
+    ],
+  },
+];
